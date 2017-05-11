9d09081b76f1c7d481c43ead0b07b869a5db2555
sunho <sunho4572@gmail.com>
2017-05-11 07:57:22 -0700

    done

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# 474a3
+# 474 a3 software design
+
+## Hello, Welcome.  
+
+This API provides you to build a pie chart more easily. All you need is your data which has one column for category and one column for corresponding numerical values. This software will create a pie chart with colored categories and their sizes will follow the values in numerical column.  
+<br /> 
+When using, just follow the following format where d[0] equals categories and d[1] equals numerical values. <i>Also please name your data as "data.csv"</i>
+<pre><code>      
+	var chart = pieChart()
+      .x(function(d) { return +d[0]; })
+      .y(function(d) { return +d[1]; });
+
+	d3.csv("data.csv", function(data) {
+	    d3.select("#vis")
+	        .datum(data)
+	        .call(chart);
+	});
+
+</code></pre>
+By doing this you will be able to create a simple pie chart. If you would like to change some features, you can use the following methods:
+<ul>
+	<li>Changing x values(categorical): chart.x(categories)</li>
+	<li>Changing y values(numerical): chart.y(numbers)</li>
+	<li>Changing the width: chart.width(w)</li>
+	<li>Changing the height: chart.height(h)</li>
+	<li>Changing the margins: chart.margin(m)</li>
+	<li>Changing colors: chart.colors(c)</li>
+</ul>
+
+To give you an example of those methods, please see below.
+<pre><code>      
+	var chart = pieChart().height(650); // changes height to 650.
+	
+</code></pre>
+
+<i>Width</i> and <i>height</i> accept a single integer, <i>x</i> and <i>y</i> accept sets of categories or numbers, <i>margin</i> accepts an object with margins, and <i>colors</i> accept a list of colors.
+
+If you have any other questions, please let me know. Thank you.
---
+++ main.js
@@ -0,0 +1,93 @@
+
+
+
+function pieChart() {
+  var margin = {top: 20, right: 20, bottom: 20, left: 20},
+      width = 600,
+      height = 600,
+      colors = ["red", "orange", "salmon", "powderblue", "green", "blue", "purple", "gray"],
+      xValue = function(d) { return d[0]; },
+      yValue = function(d) { return d[1]; };
+
+
+  var svg = d3.select("svg");
+  var g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  
+  function chart(selection) {
+    var radius = Math.min(width, height) / 2;
+    selection.each(function(data) {
+      console.log(data);
+      var pie = d3.pie()
+          .sort(null)
+          .value(function(d) { return d[Object.keys(d)[2]]; });
+
+      var path = d3.arc()
+          .outerRadius(radius - 10)
+          .innerRadius(0);
+
+      var label = d3.arc()
+          .outerRadius(radius - 50)
+          .innerRadius(radius - 40);
+      
+      d3.csv('data.csv', function(d) {
+        d[1] = +d[1];
+        return d;
+      }, function(error, data) {
+        if (error) throw error;
+      
+        var arc = g.selectAll(".arc")
+          .data(pie(data))
+          .enter().append("g")
+            .attr("class", "arc");
+
+        arc.append("path")
+            .attr("d", path)
+            .attr("fill", function(d, i) { return colors[i]; });
+
+        arc.append("text")
+            .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+            .attr("dy", "0.4em")
+            .text(function(d) { return d.data[Object.keys(d.data)[1]]; });
+      });      
+    });   
+  }; 
+  chart.x = function(xVal) {
+    if (!arguments.length) return xValue;
+    xValue = xVal;
+    return chart;
+  };
+  chart.y = function(yVal) {
+    if (!arguments.length) return yValue;
+    yValue = yVal;
+    return chart;
+  };
+
+  chart.width = function(w){
+    if (!arguments.length) return width;
+    width = w;
+    radius = Math.min(width, height) / 2;
+    return chart;
+  };
+
+  chart.height = function(h){
+    if (!arguments.length) return height;
+    height = h;
+    radius = Math.min(width, height) / 2;
+    return chart;
+  };
+
+  chart.margin = function(m) {
+    if (!arguments.length) return margin;
+    margin = m;
+    return chart;
+  }
+
+  chart.colors = function(c) {
+    if (!arguments.length) return colors;
+    colors = c;
+    return chart;
+  }
+
+  return chart;
+
+};
